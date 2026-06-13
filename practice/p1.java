071e49b301d032ce997ec26eadaa29334dc0c97d
Kartik Sehrawat <kartiksehrawat82@gmail.com>
2026-06-14 01:01:49 +0530

    Today I did inorderTraversal and maxdepth.After that I practiced abstract classes and interface

--- practice/p1.java
+++ practice/p1.java
@@ -1,15 +1,54 @@
+abstract class shape{
+    String name;
+    shape(String name){
+        this.name = name;
+    }
+    void displayInfo(){
+        System.out.println(name);
+    }
+    abstract void area();
+}
+interface drawable{
+    void draw();
+    
+}
+class circle extends shape implements drawable{
+    int radius;
+    circle(String name, int r){
+        super(name);
+        this.radius = r;
+    }
+    void area(){
+        System.out.println(3.14*2*radius);
+    }
+    public void draw(){
+        System.out.println("now drawing a circle");
+    }
+}
+class Rectangle extends shape implements drawable{
+    int length;
+    int breadth;
+    Rectangle(String name, int l,int b){
+        super(name);
+        this.length = l;
+        this.breadth = b;
+    }
+    void area(){
+        System.out.println(length*breadth);
+    }
+    public void draw(){
+        System.out.println("now drawing a rectangle");
+    }
+}
 public class p1{
     public static void main(String[]args){
-        int[] arr = {5,3,4,2,1};
-        int max = arr[0];
-        int sum = 0;
-        for (int i = 0; i < arr.length; i++){
-            System.out.print(arr[i] + " ");
-            if(max < arr[i]){
-                max = arr[i];
-            }
-            sum += arr[i];
-        }
-        System.out.println(max + " " + sum);
+        circle c = new circle("circle",2);
+        Rectangle r = new Rectangle("rectangle",3,4);
+        c.displayInfo();
+        c.draw();
+        c.area();
+        r.displayInfo();
+        r.draw();
+        r.area();
     }
 }
